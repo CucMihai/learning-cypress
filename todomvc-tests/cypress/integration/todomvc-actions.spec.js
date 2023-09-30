1e471f6349bdc5f1e45124d656975c20662b0349
Mihai Paul Cuc <Cuc.Mi.Mihai@utcluj.didatec.ro>
2023-09-30 20:49:32 +0300

    page object models lesson

--- todomvc-tests/cypress/integration/todomvc-actions.spec.js
+++ todomvc-tests/cypress/integration/todomvc-actions.spec.js
@@ -1,28 +1,33 @@
 /// <reference types="cypress" />
-// this file is created after a tutorial from TestAutomationUniversity (https://testautomationu.applitools.com/cypress-tutorial/chapter7.html)
+import {TodoPage} from '../page-objects/todo-page'
 
-describe('todo actions', ()=>{
-    beforeEach(()=>{
-        cy.visit('http://todomvc-app-for-testing.surge.sh')
-        cy.get('.todo-list').should('not.have.descendants', 'li') //this is checking if the ul list (todo.list) have no descendats
-        cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}") //this will tipe the `Clean room` test in the new-todo list,
-        //which is a label, and then it will press the enter. The enter is made by using the {enter} marker
+describe('todo actions', () => {
+  const todoPage = new TodoPage()
+
+  beforeEach(() => {
+    todoPage.navigate()
+
+    todoPage.addTodo('Clean room')
+  })
+
+  it('should add a new todo to the list', () => {
+    todoPage.validateTodoText(0, 'Clean room')
+
+    todoPage.validateToggleState(0, false)
+  })
+
+  describe('toggling todos', () => {
+    it('should toggle test correctly', () => {
+      todoPage.toggleTodo(0)
+      todoPage.validateTodoCompletedState(0, true)
     })
-    it('should add a new todo  to the list', () => {
-        
-       
-        cy.get('label').should('have.text', 'Clean room') // checks if the label have the text `Clean room`
-        cy.get('.toggle').should('not.be.checked') //checks if the toggle is not checked before
-    });
-    
-    it('should mark a todo as completed', () => {
-        cy.get('.toggle').click()
-        cy.get('label').should('have.css', 'text-decoration-line', 'line-through') 
-    });
-    
-    it('should clear the todo list', () => {
-        cy.get('.toggle').click()
-        cy.contains('Clear').click()
-        cy.get('.todo-list').should('not.have.descendants', 'li')
-    });
+
+    it('should clear completed', () => {
+      todoPage.toggleTodo(0)
+
+      todoPage.clearCompleted()
+
+      todoPage.validateNumberOfTodosShown(0)
+    })
+  })
 })
--- todomvc-tests/cypress/integration/todomvc-filtering.spec.js
+++ todomvc-tests/cypress/integration/todomvc-filtering.spec.js
@@ -1,28 +1,34 @@
-// this file is created after a tutorial from TestAutomationUniversity (https://testautomationu.applitools.com/cypress-tutorial/chapter7.html)
 /// <reference types="cypress" />
+import {TodoPage} from '../page-objects/todo-page'
 
-describe('filtering',()=>{
-    beforeEach(()=>{
-        cy.visit('http://todomvc-app-for-testing.surge.sh')
-        cy.get('.todo-list').should('not.have.descendants', 'li') // this is checking if the ul list (todo.list) have no descendats
-        cy.get('.new-todo').type("Clean room{enter}")
-        cy.get('.new-todo').type("Learn JS{enter}")
-        cy.get('.new-todo').type("Use Cypress{enter}")
+describe('filtering', function() {
+  const todoPage = new TodoPage()
 
-        cy.get('.todo-list li:nth-child(2) .toggle').click() // if you look into this selector, you can see that the selection is 
-        // pretty much the same as xpath, they go deeper into the element. The .todo-list have li as elements and a toggle
-        // cy.get(':nth-child(2) > .view > .toggle').click()
-    })
-    it('it should filter active todos', () => {
-        cy.contains('Active').click()
-        cy.get('.todo-list li').should('have.length',2)
-    });
-    it('it should filter active todos', () => {
-        cy.contains('All').click()
-        cy.get('.todo-list li').should('have.length',3)
-    });
-    it('it should filter active todos', () => {
-        cy.contains('Completed').click()
-        cy.get('.todo-list li').should('have.length',1)
-    });
+  beforeEach(() => {
+    todoPage.navigate()
+
+    todoPage.addTodo('Clean room')
+    todoPage.addTodo('Learn JavaScript')
+    todoPage.addTodo('Use Cypress')
+
+    todoPage.toggleTodo(1)
+  })
+
+  it('should filter "Active" correctly', () => {
+    todoPage.showOnlyActiveTodos()
+
+    todoPage.validateNumberOfTodosShown(2)
+  })
+
+  it('should filter "Completed" correctly', () => {
+    todoPage.showOnlyCompletedTodos()
+
+    todoPage.validateNumberOfTodosShown(1)
+  })
+
+  it('should filter "All" correctly', () => {
+    todoPage.showAllTodos()
+
+    todoPage.validateNumberOfTodosShown(3)
+  })
 })
